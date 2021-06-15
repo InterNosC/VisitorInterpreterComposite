b826f7f7f63db54b66a900768da05a2897c7857b
InterNos <kirilrknu@gmail.com>
2021-06-15 11:25:04 +0300

    Add project files.

---
+++ Visitor/Interpreter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Visitor
+{
+
+    class Context
+    {
+    }
+
+    abstract class AbstractExpression
+    {
+        public abstract void Interpret(Context context);
+    }
+
+    class TerminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+        }
+    }
+
+    class NonterminalExpression : AbstractExpression
+    {
+        AbstractExpression expression1;
+        AbstractExpression expression2;
+        public override void Interpret(Context context)
+        {
+
+        }
+    }
+}
---
+++ Visitor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Read();
+        }
+    }
+
+}
